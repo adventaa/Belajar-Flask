63c79a93b7983e574a312e96f45b240e87ae8690
adventaa <fransiska.adventa@gmail.com>
2025-09-22 20:16:32 +0700

    template jinja dengan IF dan LOOP

---
+++ web7.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask ("__name__")
+
+@app.route("/")
+def home():
+    is_logged_in = True
+    nama = "adventa"
+    items = ['Mangga', 'Jeruk', 'nanas']
+    return render_template('index2.html', is_logged_in = is_logged_in, nama = nama, items = items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
